238f121576dcbfe2152d865c816dc8d5b7cacdad
mreleftheros <eleftheros_all@protonmail.com>
2024-06-18 20:59:00 +0300

    write logic for fizzbuzz

--- src/main.zig
+++ src/main.zig
@@ -7,5 +7,19 @@ pub fn main() !void {
 
     try stdout.print("Hello {s}\n", .{"world"});
 
+    var i: u8 = 0;
+
+    while (i <= 100) : (i += 1) {
+        if (i % 3 == 0 and i % 5 == 0) {
+            try stdout.print("FizzBuzz\n", .{});
+        } else if (i % 3 == 0) {
+            try stdout.print("Fizz\n", .{});
+        } else if (i % 5 == 0) {
+            try stdout.print("Buzz\n", .{});
+        } else {
+            try stdout.print("{d}\n", .{i});
+        }
+    }
+
     try bw.flush();
 }
